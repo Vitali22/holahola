cda69ee02c206d9e19054bba7af90bebd022c204
Emilio Vitali <esocconini@gmail.com>
2022-03-16 16:40:01 -0600

    first commiasdasd

---
+++ index.js
@@ -0,0 +1,12 @@
+const options = {
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+    'x-apikey': '167a543753285376fa59206b24a4ee524b39926295db48458478364d017bd9dd'
+  }
+};
+
+fetch('https://www.virustotal.com/api/v3/domains/programdiag.com', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
