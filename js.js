59fcbb23a6d65d3085aa624e0ed73b94fd81c94b
Emilio Vitali <esocconini@gmail.com>
2022-03-16 17:52:02 -0600

    sadfasdfoiasdasd

--- js.js
+++ js.js
@@ -1,21 +1,14 @@
-  function JSONRequest(){
-    const options = {
-    method: 'GET',
-    headers: {
+const options = {
+  method: 'GET',
+  headers: {
     Accept: 'application/json',
     'x-apikey': '167a543753285376fa59206b24a4ee524b39926295db48458478364d017bd9dd'
   }
 };
 
-    let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    let request = new XMLHttpRequest();
-    request.open('https://www.virustotal.com/api/v3/ip_addresses/4.2.2.2', options)
-    request.send();
-    request.onload = () => {
-    let JSONFile = JSON.parse(request.responseText);
-    let downloads = JSON.parse(JSONFile.stats.downloads);
-    document.getElementById("output").innerHTML = downloads;
-    }
-}
+request.open('https://www.virustotal.com/api/v3/ip_addresses/4.2.2.2', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+document.getElementById('elcoso').innerHTML = response["attributes"]["last_analysis_stats"]["malicious"];
 
-JSONRequest();
