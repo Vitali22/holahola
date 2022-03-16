30cc4b548ec0a5e85283a861553e86d2c17973d8
Emilio Vitali <esocconini@gmail.com>
2022-03-16 17:31:01 -0600

    sadfasdfoiasdsa

--- js.js
+++ js.js
@@ -1,12 +1,21 @@
-const options = {
-  method: 'GET',
-  headers: {
+  function JSONRequest(){
+    const options = {
+    method: 'GET',
+    headers: {
     Accept: 'application/json',
     'x-apikey': '167a543753285376fa59206b24a4ee524b39926295db48458478364d017bd9dd'
   }
 };
 
-fetch('https://www.virustotal.com/api/v3/ip_addresses/4.2.2.2', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+    let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    let request = new XMLHttpRequest();
+    request.open('https://www.virustotal.com/api/v3/ip_addresses/4.2.2.2', options)
+    request.send();
+    request.onload = () => {
+    let JSONFile = JSON.parse(request.responseText);
+    let downloads = JSON.parse(JSONFile.stats.downloads);
+    document.getElementById("output").innerHTML = downloads;
+    }
+}
+
+JSONRequest();
