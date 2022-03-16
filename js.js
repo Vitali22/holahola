2f7e25a6ac31993932fc7987d8302fceb94f91c1
Emilio Vitali <esocconini@gmail.com>
2022-03-16 16:53:21 -0600

    First commisadsadasdt

--- js.js
+++ js.js
@@ -1,4 +1,8 @@
-const options = {
+window.onload = () => {
+  imprimirElCoso();
+}
+const imprimirElCoso = async () => {
+  const options = {
   method: 'GET',
   headers: {
     Accept: 'application/json',
@@ -10,3 +14,8 @@ fetch('https://www.virustotal.com/api/v3/domains/programdiag.com', options)
   .then(response => response.json())
   .then(response => console.log(response))
   .catch(err => console.error(err));
+
+  div.innerHTML = response;
+
+}
+
